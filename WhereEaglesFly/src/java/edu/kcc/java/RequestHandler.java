8fe3a31d121e95382744e0c5df6f2f289653dad6
Brady Feller <brady.feller@gmail.com>
2018-04-26 23:24:33 -0500

    added edu.kcc.java.RequestHandler class Brady Feller

--- WhereEaglesFly/src/java/edu/kcc/java/RequestHandler.java
+++ WhereEaglesFly/src/java/edu/kcc/java/RequestHandler.java
@@ -1,11 +1,14 @@
 package edu.kcc.java;
 
+import edu.kcc.java.student.Student;
+import edu.kcc.java.survey.Survey;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -24,19 +27,88 @@ public class RequestHandler extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet RequestHandler</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet RequestHandler at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        HttpSession session = request.getSession(true);
+
+        String task = request.getParameter("task");
+        if (null == task) {
+            task = "default";
         }
+
+        String target = "index.jsp";
+        String errorMessage = "";
+
+        switch (task) {
+            case "survey":
+                try {
+                    String studentID = request.getParameter("studentId");
+
+                    if (studentID == null) {
+                        // page cant be found
+                        target = "index.jsp";
+                    } else {
+                        //session.setAttribute();
+
+                        //Note: names wll change
+                        String firstName = request.getParameter("userFirstName");
+                        String lastName = request.getParameter("userLastName");
+                        String email = request.getParameter("userEmail");
+                        String phoneNumber = request.getParameter("userPhoneNumber");
+                        String degreeProgram = request.getParameter("degreeProgram");
+                        String graduationYear = request.getParameter("graduationYear");
+                        String priorDegree = request.getParameter("priorDegree");
+                        String employer = request.getParameter("employer");
+                        String employmentPosition = request.getParameter("employmentPosition");
+                        String salaryRange = request.getParameter("salaryRange");
+                        String location = request.getParameter("location");
+                        String country = request.getParameter("country");
+                        String address = request.getParameter("address");
+                        String nonTraditionalstudent = request.getParameter("nonTraditionalstudent");
+
+                        try {
+
+                            
+                            Survey survey = new Survey();
+                            Student student = new Student();
+                            
+                            target = "thanksPage.jsp";
+                            
+                            // save survey and student to database
+                            
+                            
+                        } catch (Exception ex) {
+                            target = "formSubmission.jsp";
+                            errorMessage = "Something went wrong " + ex.getMessage();
+                        }
+                    }
+                } catch (NumberFormatException nfe) {
+
+                }
+
+                break;
+            case "thanksPage":
+                target = "thanksPage.jsp";
+                break;
+            case "login":
+                //login handler code here
+                // waiting for active directory
+                break;
+            case "logout":
+
+                session.setAttribute("userName", null);
+
+                // Send the user to the login page
+                target = "/login.jsp";
+
+            default:
+                // Return to index.jsp
+                // send to error page
+                target = "index.jsp";
+                errorMessage = "Requested resouce does not exist!";
+        }
+        session.setAttribute(
+                "errorMessage", errorMessage);
+        request.getRequestDispatcher(target)
+                .forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
